7133caae1fcc9ea58a4c59874d92a3533b85182f
Sami Mäkelä <sami@httpster.io>
2022-07-18 22:17:39 +0300

    removed read time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.md', './src/**/*.html', './src/_includes/**/*.njk'],
+  content: ['./src/**/*.md', './src/**/*.html', './src/**/*.njk'],
   plugins: [
     require('@tailwindcss/typography')
   ],
